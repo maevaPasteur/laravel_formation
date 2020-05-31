00a36c29d04f8920da43759e7a9310840023f221
MathisDYKDan <mathis.dyk@danparis.fr>
2020-05-31 20:20:09 +0200

    Add inscription

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $session->users }}
+
     <h2>Session {{ $session->start  }}</h2>
     <p>Formation : {{ $session->formation->title }}</p>
-    <a href="{{ route('sessions.inscription', $session)  }}">S'inscrire</a>
+
+    <h3>Liste des inscrits</h3>
+    <ul>
+    @foreach($session->users as $user)
+        <li>{{ $user->name }}</li>
+    @endforeach
+    </ul>
+
+    @if(!$session->users->contains(auth()->user()))
+        <a href="{{ route('sessions.inscription', $session)  }}">S'inscrire</a>
+    @endif
 
 @endsection
