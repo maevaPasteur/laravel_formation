6e948f499d2fbec37451ff5b6b684532d63c03dc
MaevaPasteur <maeva.pasteur.pro@gmail.com>
2020-06-02 00:11:33 +0200

    Feat login register style

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -24,7 +24,7 @@ class SessionController extends Controller
      */
     public function index()
     {
-        $sessions = Session::all();
+        $sessions = Session::latest()->paginate(9);
         return view('sessions.index', compact('sessions'));
     }
 
