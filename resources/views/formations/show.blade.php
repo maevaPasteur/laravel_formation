03a4bd5d6b04178d0a1f37652246cc055c3430a3
MathisDYKDan <mathis.dyk@danparis.fr>
2020-06-03 23:19:01 +0200

    fix commit

--- resources/views/formations/show.blade.php
+++ resources/views/formations/show.blade.php
@@ -11,12 +11,8 @@
             <p class="fw-4">{{ $formation->description }}</p>
             <br>
             <p>{{ $formation->content }}</p>
-<<<<<<< HEAD
-            <p class="mb-20">Formation gérée par {{  $formation->user->name }}</p>
+            <p class="mb-20">Formation gérée par {{ $formation->user->name }}</p>
 
-=======
-            <p class="mb-20">Formation proposée par {{  $formation->user->name }}</p>
->>>>>>> 83fe844bef68f41bfa662f0fcc4aa8e182f0e9a2
             <div class="d-flex">
                 @can('update', $formation)
                     <a class="btn dark mr-20" href="{{ route('formations.edit', $formation) }}">Modifier la formation</a>
