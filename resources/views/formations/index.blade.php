bcaf4f7cac0e9d3a0952c993ad0a421ac3303988
Maëva Pasteur <maeva.pasteur.pro@gmail.com>
2020-06-03 21:58:58 +0200

    Update index formation without data

--- resources/views/formations/index.blade.php
+++ resources/views/formations/index.blade.php
@@ -12,70 +12,75 @@
     </section>
     <section class="wrapper container_formations">
             <h2>La liste des formations</h2>
-            <ul>
-            @foreach($formations as $formation)
-                <li>
-                    <article>
-                        <a href="{{ route('formations.show', $formation) }}">
-                            <h3>Formation en {{ $formation->title }}</h3>
-                            @if (count($formation->categories) > 0)
-                                <div class="d-flex">
-                                    @foreach($formation->categories as $category)
-                                        <span class="tag">{{ $category->name }}</span>
-                                    @endforeach
-                                </div>
-                            @endif
-                            <p>{{ $formation->description }}</p>
-                            <button class="btn yellow">Voir la formation</button>
-                        </a>
-                    </article>
-                </li>
-            @endforeach
-            </ul>
-        {{ $formations->links() }}
+            @if ($formations->count() > 0)
+                <ul>
+                @foreach($formations as $formation)
+                    <li>
+                        <article>
+                            <a href="{{ route('formations.show', $formation) }}">
+                                <h3>Formation en {{ $formation->title }}</h3>
+                                @if (count($formation->categories) > 0)
+                                    <div class="d-flex">
+                                        @foreach($formation->categories as $category)
+                                            <span class="tag">{{ $category->name }}</span>
+                                        @endforeach
+                                    </div>
+                                @endif
+                                <p>{{ $formation->description }}</p>
+                                <button class="btn yellow">Voir la formation</button>
+                            </a>
+                        </article>
+                    </li>
+                @endforeach
+                </ul>
+                {{ $formations->links() }}
+            @else
+                <p>Aucune formation pour le moment</p>
+            @endif
     </section>
-    <section class="wrapper">
-        <h2>Les prochaines sessions</h2>
-        @if($sessions->count() > 0)
-        <table class="container_calendar">
-            <thead class="top">
-            <tr>
-                <th colspan="7">
-                    <a href="/?month={{ $monthnb - 1 }}&year={{ $year }}"> < </a>
-                    <span class="headcal">{{ $month.' '.$year }}</span>
-                    <a href="/?month={{ $monthnb + 1 }}&year={{ $year }}"> > </a>
-                </th>
-            </tr>
-            <tr>
-                @for ($i = 1; $i <= 7; $i++)
-                    <th>{{ $daytab[$i] }}</th>
-                @endfor
-            </tr>
-            </thead>
-            <tbody>
-            @for ($i = 1; $i <= count($calendar); $i++)
+
+    @if($sessions->count() > 0)
+        <section class="wrapper">
+            <h2>Les prochaines sessions</h2>
+            <table class="container_calendar">
+                <thead class="top">
                 <tr>
-                    @for($j = 1; $j <= 7 && $j-$z+1+(($i*7)-7) <= $nbdays; $j++)
-                        @php $day_current = $formation->day_current($j, $z, $i, $monthnb, $year, $calendar) @endphp
-                        @if($day_current == '')
-                            <td><p>{{ $day_current }}</p></td>
-                        @else
-                            <td class="{{ $formation->day_class($j, $z, $i, $monthnb, $year, $calendar) }}">
-                                <p>{{ $day_current }}</p>
-                                @foreach($formation->sessions_this_day($sessions, $day_current, $monthnb, $year) as $session)
-                                    <a class="session" href="/sessions/{{ $session->id }}">
-                                        <span>{{ $formations->where('id', $session->formation_id)->first()->title }}</span><br>
-                                        {{ $classrooms->where("id", $session->classroom_id)->first()->places - $session->users->count() }} places dispo
-                                    </a>
-                                @endforeach
-                            </td>
-                        @endif
+                    <th colspan="7">
+                        <a href="/?month={{ $monthnb - 1 }}&year={{ $year }}"> < </a>
+                        <span class="headcal">{{ $month.' '.$year }}</span>
+                        <a href="/?month={{ $monthnb + 1 }}&year={{ $year }}"> > </a>
+                    </th>
+                </tr>
+                <tr>
+                    @for ($i = 1; $i <= 7; $i++)
+                        <th>{{ $daytab[$i] }}</th>
                     @endfor
                 </tr>
-            @endfor
-            </tbody>
-        </table>
-        @endif
-    </section>
+                </thead>
+                <tbody>
+                @for ($i = 1; $i <= count($calendar); $i++)
+                    <tr>
+                        @for($j = 1; $j <= 7 && $j-$z+1+(($i*7)-7) <= $nbdays; $j++)
+                            @php $day_current = $formation->day_current($j, $z, $i, $monthnb, $year, $calendar) @endphp
+                            @if($day_current == '')
+                                <td><p>{{ $day_current }}</p></td>
+                            @else
+                                <td class="{{ $formation->day_class($j, $z, $i, $monthnb, $year, $calendar) }}">
+                                    <p>{{ $day_current }}</p>
+                                    @foreach($formation->sessions_this_day($sessions, $day_current, $monthnb, $year) as $session)
+                                        <a class="session" href="/sessions/{{ $session->id }}">
+                                            <span>{{ $formations->where('id', $session->formation_id)->first()->title }}</span><br>
+                                            {{ $classrooms->where("id", $session->classroom_id)->first()->places - $session->users->count() }} places dispo
+                                        </a>
+                                    @endforeach
+                                </td>
+                            @endif
+                        @endfor
+                    </tr>
+                @endfor
+                </tbody>
+            </table>
+        </section>
+    @endif
 
 @endsection
